5fa15a5b677a837b048b375c20d33044b7218174
jxd9612 <15916537217@163.com>
2020-04-23 17:48:13 +0800

    完善

--- miniprogram/components/playList/index.js
+++ miniprogram/components/playList/index.js
@@ -1,10 +1,44 @@
+// const app = getApp();
+const computedBehavior = require('miniprogram-computed');
+
 Component({
+    behaviors: [computedBehavior],
+
     // 组件的属性列表
-    properties: {},
+    properties: {
+        item: {
+            type: Object,
+            value: {},
+        },
+        index: {
+            type: Number,
+        },
+        // 歌曲 Id ，与 item.id 判断当前点击的歌曲条目
+        songId: {
+            type: Number,
+        },
+        // 歌曲状态
+        playStatus: {
+            type: Number,
+        },
+    },
 
     // 组件的初始数据
-    data: {},
+    data: {
+        desc: '',
+        status: false,
+    },
+
+    watch: {
+        item(val) {
+            if (val) this.setData({ desc: this.dealDesc(val.alia) });
+        },
+    },
 
     // 组件的方法列表
-    methods: {},
+    methods: {
+        dealDesc(alia) {
+            return alia.length == 0 ? '' : `${alia[0]}`;
+        },
+    },
 });
