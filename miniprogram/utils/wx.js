747fbc0f3f34798793b7c54ef583ecba0b7909bb
jxd9612 <15916537217@163.com>
2020-04-26 17:32:22 +0800

    微信音乐APII

--- miniprogram/utils/wx.js
+++ miniprogram/utils/wx.js
@@ -2,7 +2,10 @@ const showLoading = (title = '加载中...', mask = true) => wx.showLoading({ ti
 
 const hideLoading = (wait = 500) => setTimeout(wx.hideLoading, wait);
 
+const musicManage = wx.getBackgroundAudioManager();
+
 module.exports = {
     showLoading,
     hideLoading,
+    musicManage,
 };
